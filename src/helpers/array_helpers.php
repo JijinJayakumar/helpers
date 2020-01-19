eb4a1d0b57b361ab73001eec0393137fe7cb702d
Jijin <jijinjayakumaronline@gmail.com>
2020-01-19 09:57:53 +0530

    adeed new array helpers

--- src/helpers/array_helpers.php
+++ src/helpers/array_helpers.php
@@ -65,3 +65,59 @@ if (!function_exists('arrayOrderBy')) {
         return array_values($sortedItems);
     }
 }
+
+if (!function_exists('arrayVariadicSum')) {
+/**
+ * arrayVariadicSum
+ *Takes an indefinite number of integers and returns their sum
+ * @param  mixed $numbers
+ *
+ * @return void
+ * example variadicSum(1, 2);
+
+ */
+    function arrayVariadicSum(...$numbers)
+    {
+        $sum = 0;
+        foreach ($numbers as $num) {
+            $sum += $num;
+        }
+        return $sum;
+    }
+}
+
+if (!function_exists('arrayReject')) {
+/**
+ * arrayReject
+ * Filters the collection using the given callback.
+ * @param  array $items
+ * @param  function $func
+ *
+ *reject(['Apple', 'Pear', 'Kiwi', 'Banana'], function ($item) {
+ *return strlen($item) > 4;
+ *}); // ['Pear', 'Kiwi']
+ *
+ * @return void
+ */
+    function arrayReject($items, $func)
+    {
+        return array_values(array_diff($items, array_filter($items, $func)));
+    }
+
+}
+
+if (!function_exists('arrayTake')) {
+
+/**
+ * arrayTake
+ *Returns an array with n elements removed from the beginning.
+ * @param  mixed $items
+ * @param  mixed $n
+ *
+ * @return void
+ */
+    function arrayTake($items, $remove_from_beginning_index = 1)
+    {
+        return array_slice($items, 0, $remove_from_beginning_index);
+    }
+}
